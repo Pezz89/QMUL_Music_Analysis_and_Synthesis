bc362eccb2a1eb19f3d043c259f8b1583fbe15be
Sam Perry <u1265119@unimail.hud.ac.uk>
2017-03-08 18:11:55 +0000

    transcription matrix is now saved as mat file by running back_pr1.m

--- main.m
+++ main.m
@@ -1,2 +1,8 @@
 function main()
-    fileName = '../ass2-resources/';
+    midiFileName = './media/bach_pr1.mid';
+    wav1FileName = './media/bach_pr1_a.wav';
+    wav2FileName = './media/bach_pr1_b.wav';
+    f = load('./media/notes.mat');
+    transcriptionMatrix = f.notes
+
+    [f0 B] = fundamentals(wav1FileName, transcriptionMatrix)
